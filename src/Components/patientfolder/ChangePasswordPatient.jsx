ccc1810751cdde616d8b2f4e3442299610e39291
akshay <100370723+akshayshinde02@users.noreply.github.com>
2024-05-11 11:57:10 +0530

    add

--- src/Components/patientfolder/ChangePasswordPatient.jsx
+++ src/Components/patientfolder/ChangePasswordPatient.jsx
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 
-export default function ChangepasswordPatient() {
+export default function ChangePasswordPatient() {
   const [patientInfo, setPatientInfo] = useState("");
   const [userInfo, setUserInfo] = useState("");
 
